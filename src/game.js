95363c39799b7258c0af9038b84191cdaef549ad
cynthia <118978961+cyntrist@users.noreply.github.com>
2023-11-01 20:26:29 +0100

    aa

--- src/game.js
+++ src/game.js
@@ -1,19 +1,34 @@
 import TypeWriterDemo from './typewritter.js';
 
-const config = {
+/**
+ * Inicio del juego en Phaser. Creamos el archivo de configuración del juego y creamos
+ * la clase Game de Phaser, encargada de crear e iniciar el juego.
+ * Doc: https://photonstorm.github.io/phaser3-docs/Phaser.Types.Core.html#.GameConfig
+ */
+
+let config = {
   width: 1280,              
   height: 720,            
-  type: Phaser.AUTO,      
+  type: Phaser.CANVAS,      
   parent: document.getElementById('juego'),
   scale: {
+    /* cyn: las siguientes tres lineas son la única combinación que he encontrado para que escale bien*/
     parent: 'juego',
-    mode: Phaser.Scale.WIDTH_CONTROLS_HEIGHT ,
+    mode: Phaser.Scale.WIDTH_CONTROLS_HEIGHT,
     autoCenter: Phaser.Scale.CENTER_BOTH, 
+    min: { // cyn: esto no funciona para absolutamente nada (probablemente por lo anterior) pero me gusta soñar
+          width: 640,
+          height: 480
+      },
+    max: {
+          width: 1920,
+          height: 1080
+      }
   },
-  scene: TypeWriterDemo,
+  scene: [TypeWriterDemo],
   title: "MBTI",
   version: "1.0.0"
 }
 
-var game = new Phaser.Game(config);
+new Phaser.Game(config);
  
