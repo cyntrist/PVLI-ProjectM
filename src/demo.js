adabd34974b7f795f35048f65eb535a6e13d1ea3
Inés <ineprimo@ucm.es>
2023-11-01 11:35:40 +0100

    se pone la fotillo ayyyyy

--- src/demo.js
+++ src/demo.js
@@ -6,25 +6,26 @@ export default class Demo extends Phaser.Scene {
 	 * Constructor, con key  --->
 	 */
 	constructor() {
-		super({ key: 'demo' });
+		super('demo');
 	}
 
-	init() {
-
-	}
-
+	
 	preload() {
 		// carga la flecha
 		this.load.image('arrow', 'assets/images/escenarios/flecha.png');
 
 		// carga los fondos
-		this.load.image('clase', 'assets/images/escenarios/clase.png');
-		this.load.image('pasillo', 'assets/images/escenarios/pasillo.png');
+		this.load.image('clase', './assets/images/escenarios/clase2.jpg');
+
+
+		this.load.image('pasillo', 'assets/images/escenarios/pasillo.jpg');
 	}
 
 	create() {
 		// fondo
-		this.add.image(0, 0, 'clase').setScale(0.8, 0.8).setOrigin(0, 0) // el fondo
+		this.add.image(0, 0, 'clase').setScale(0.35, 0.35).setOrigin(0, 0); // el fondo
+
+
 
 		//new Arrow();
 
