bda61da96a19a1bf0a065d914faff86abcc037ac
Paulalemany <palemany@ucm.es>
2023-12-05 17:13:54 +0100

    intentando hacer feedback (sale mal)

--- src/objects/decisionButton.js
+++ src/objects/decisionButton.js
@@ -18,7 +18,7 @@ export default class DecisionButton extends Phaser.GameObjects.Container {
      * @param {int} corner - tamaño del frame del 3-slice
      * @param {int} id - numero en el array de la opcion
 	*/
-    constructor(scene, x, y, sprite, width, height, text, corner, value) {
+    constructor(scene, x, y, sprite, width, height, text, corner, value, {EnterCallback, ExitCallback}) {
 		// super a la escena
 		super(scene, x, y);
 
@@ -43,9 +43,23 @@ export default class DecisionButton extends Phaser.GameObjects.Container {
             }
         );
         this.text.setOrigin(0.5, 0.5);
+
         this.nineslice.on('pointerdown', function(){
             scene.eventEmitter.emit('decided', value);
         })
+
+        this.nineslice.on('pointerover', function(){
+            console.log("Encima");
+           this.nineslice.sprite.tint = "0xc24d6d"
+        })
+
+        //Reconoce que el ratón está encima de la función
+		if (EnterCallback)
+        this.box.on('pointerover', EnterCallback);
+
+        //Reconoce que el raton sale del botón
+        if (ExitCallback)
+        this.box.on('pointerout', ExitCallback);
 
         this.on('destroy', function onDestroy() {
 			console.log("ME MUEROOOO");
