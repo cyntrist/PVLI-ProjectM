f936ed08dede95612eff0afb10080aa5dee53b90
David-104 <118064911+David-104@users.noreply.github.com>
2023-12-09 04:02:40 +0100

    mejor dejo esto como estaba antes xd

--- src/scenes/demo.js
+++ src/scenes/demo.js
@@ -33,7 +33,7 @@ export default class Demo extends Phaser.Scene
 		this.load.image('box', './assets/images/escenarios/opciones2.png')
 		this.load.image('movil', './assets/images/movil/movil.png');
 
-		this.load.json('dia1Data', './assets/dialogue editor/Dialog files/test_afinidad.json')
+		this.load.json('dia1Data', './src/json/dia1_test.json')
 		this.load.image('9slice', './assets/images/ui/botones_decision_nineslice_muy_peque.png')
     }
 
