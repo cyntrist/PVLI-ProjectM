a610863feffc58d9194368be4ddccdfc88067800
cyntrist <118978961+cyntrist@users.noreply.github.com>
2023-12-10 06:06:42 +0100

    adopfijaewopfjaosdjfpoaijwefoijasdflkñjas oifjpaewo opifjoawef

--- src/scenes/demo.js
+++ src/scenes/demo.js
@@ -1,7 +1,7 @@
 import DialogText from "../plugins/dialog_plugin.js";
 import Character from "../objects/character.js"
 import Button from "../objects/button.js";
-import Decision from "../objects/decision.js";
+import DialogueManager from "../managers/dialogManager.js"
 
 let Scenary = 'clase';
 /**
@@ -43,7 +43,6 @@ export default class Demo extends Phaser.Scene
 		const { width, height } = this.canvas; // la anchura y altura del canvas
 		const scene = this // referencia a esta misma escena
 		this.width = width; this.height = height;
-		let title = "\n\n\                                                                                            <3 MY BELOVED TRUE INTEREST <3";
 		const Elenco = { Camille: 0, Delilah: 1, Matthew: 2, Richard: 3} // enum para que queden más bonitas las asignaciones pero vamos es una tonteria
 		const padding = 40; // espacio respecto al origen
 		const sprites = [ // array de sprites
@@ -71,23 +70,7 @@ export default class Demo extends Phaser.Scene
 
 		
 		// ** CREACION DE INTERFAZ ** //
-		// crea la ventana de diálogo
-		scene.dialog = new DialogText(this, {
-			borderThickness: 6,
-			borderColor: 0xF6F6F6,
-			borderAlpha: 0.8,
-			windowBorderRadius: 4,
-			windowAlpha: 0.95,
-			windowColor: 0xFF799A,
-			windowHeight: 150,
-			padding: 18,
-			hasCloseBtn: false,
-			closeBtnColor: 'white',
-			dialogSpeed: 4.4,
-			fontSize: 24,
-			fontFamily: "lato"
-		});
-		scene.dialog.depth = 2;
+
 
 		// crea un boton al pasillo
 		let but1 = new Button(this, 590, 200, 'pasillo', 2, 'box', { "ClickCallback": () => this.ChangeScenary ("pasillo", scene),
@@ -120,46 +103,26 @@ export default class Demo extends Phaser.Scene
 			persist: true
 		})
 	
-
-		// ** DIALOGO MOMENTO: ESTO DEBERÁ IR EN EL DIALOGUE MANAGER EN EL FUTURO ** //
-		let i = 0;
-		let node = dayData.root.next; // primer nodo
-		let decision;
-		scene.dialog.setText(title, true); // imprime la línea de título
-		scene.dialog.graphics.on('pointerdown', function () { // cada click 
-			decision?.destroy();
-			let currentNode = dayData[node];
-			let currentName = currentNode.name.toLowerCase();
-			let currentCharacter = characters[currentName]; 
-			currentCharacter?.onFocus(); //muy importante el interrogante 
-			currentCharacter?.unfocusEveryoneElse(characters);
-			if (currentNode.hasOwnProperty("next") || currentNode.hasOwnProperty("choices")) { // si es un nodo intermedio y/o tiene tiene elecciones
-				if ( i < 1) 
-					scene.dialog.setText(currentNode.name + ":\n" + currentNode.text.es, true);
-				if (currentNode.hasOwnProperty("choices")) { 
-					if (i >= 1) { // manera muy guarra de necesitar dos clics antes de que aparezca la decision
-						decision = new Decision(scene, currentNode.choices, '9slice');
-						i = 0;
-						scene.dialog.setInteractable(false);
-					} 
-					else i++;
-				}
-				else node = currentNode.next; // si no hay decisiones, continuación lineal, el nodo actual pasa a ser el siguiente
-			}
-			else {
-				scene.dialog.setText(currentNode.text.es); // se escribe el último msj
-				camille.unfocusEveryone(characters);
-			}
-		})		
-
-		this.eventEmitter.on('decided', function (valor) {
-			console.log('OPCION DECIDIDA: ', valor);
-			camille.focusEveryone(characters);
-			scene.dialog?.setText("T/N:\n" + dayData[node].choices[valor].text.es, true);
-			node = dayData[node].choices[valor].next;
-			scene.dialog.setInteractable(true);
-			decision.destroy();
+		// creacion de la ventana de diálogo
+	    scene.dialog = new DialogText(this, {
+			borderThickness: 6,
+			borderColor: 0xF6F6F6,
+			borderAlpha: 0.8,
+            windowBorderRadius: 4,
+			windowAlpha: 0.95,
+			windowColor: 0xFF799A,
+			windowHeight: 150,
+			padding: 18,
+            hasCloseBtn: false,
+            closeBtnColor: 'white',
+			dialogSpeed: 4.4,
+			fontSize: 24,
+			fontFamily: "lato"
 		});
+		scene.dialog.depth = 2;
+		
+		// creacion del manager de dialogo
+		let dialogManager = new DialogueManager(scene, dayData, characters, '9slice');
     }
 
 	update(){
