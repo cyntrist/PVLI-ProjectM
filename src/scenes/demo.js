8775c0131073498b6d69dc733857f9c2539cfdc0
cyntrist <118978961+cyntrist@users.noreply.github.com>
2023-12-18 15:53:42 +0100

    Update demo.js

--- src/scenes/demo.js
+++ src/scenes/demo.js
@@ -1,4 +1,3 @@
-import DialogText from "../plugins/dialog_plugin.js";
 import Character from "../objects/character.js"
 import Button from "../objects/button.js";
 import PlayerManager from "../managers/playerManger.js";
@@ -69,7 +68,13 @@ export default class Demo extends Phaser.Scene
 		const day2mData = this.cache.json.get('dia2mData'); 
 		const day3mData = this.cache.json.get('dia3mData'); 
 		const day4mData = this.cache.json.get('dia4mData'); 
-		const dayDatas = { day1mData, day2mData, day3mData, day4mData }; // !!!!!!!!!IMPORTANTE!! añadir aquí el resto de jsons que se generen 
+		const dayDatas = { 
+			day1mData, 
+			day1mdData,
+			day2mData, 
+			day3mData, 
+			day4mData 
+		}; // !!!!!!!!!IMPORTANTE!! añadir aquí el resto de jsons que se generen 
 		
 		// Parámetros de la escena
 		const { width, height } = this.canvas; // la anchura y altura del canvas
@@ -78,32 +83,29 @@ export default class Demo extends Phaser.Scene
 		const padding = 40; // espacio respecto al origen
 
 		// Parámetros de los personajes
-		const Elenco = { Camille: 0, Delilah: 1, Matthew: 2, Richard: 3} // enum para que queden más bonitas las asignaciones pero vamos es una tonteria
 		const sprites = [ // array de sprites
 			scene.add.sprite(0, padding, 'camilleph').setScale(0.5,0.5),
 			scene.add.sprite(0, padding, 'delilahph').setScale(0.5,0.5),
 			scene.add.sprite(0, padding, 'matthewph').setScale(0.5,0.5),
 			scene.add.sprite(0, padding, 'richardph').setScale(0.5,0.5)
 		]
-
 		// Creación de personajes
-		const camille = new Character(scene, width*1/5, height - sprites[Elenco.Camille].displayHeight/2, sprites[Elenco.Camille], "Camille", 0);
-		const delilah = new Character(scene, width*2/5, height - sprites[Elenco.Delilah].displayHeight/2, sprites[Elenco.Delilah], "Delilah", 1);
-		const matthew = new Character(scene, width*3/5, height - sprites[Elenco.Matthew].displayHeight/2, sprites[Elenco.Matthew], "Matthew", 2);
-		const richard = new Character(scene, width*4/5, height - sprites[Elenco.Richard].displayHeight/2, sprites[Elenco.Richard], "Richard", 3);
-		// diccionario de los personajes, pensado para las iteraciones dentro de Character
+		const camille = new Character(scene, width*1/5, height - sprites[0].displayHeight/2, sprites[0], "Camille", 0);
+		const delilah = new Character(scene, width*2/5, height - sprites[1].displayHeight/2, sprites[1], "Delilah", 1);
+		const matthew = new Character(scene, width*3/5, height - sprites[2].displayHeight/2, sprites[2], "Matthew", 2);
+		const richard = new Character(scene, width*4/5, height - sprites[3].displayHeight/2, sprites[3], "Richard", 3);
+		// DICCIONARIO (!IMPORTANTE!) de los personajes
 		const characters = { camille, delilah, matthew, richard } // corchetes array, brackets diccionario (objeto)
 		// descolorea a todos los personajes antes de empezar
 		camille.unfocusEveryone(characters); // camille siendo conejillo de indias
-
+		
+		// ** CREACION DE INTERFAZ ** //
 		// pone el fondo
 		this.nextBG = 1;
 		this.currentBG = 1;
-        let bg = scene.add.image(0, 0, "clase").setScale(0.35, 0.35).setOrigin(0, 0);
+		let bg = scene.add.image(0, 0, "clase").setScale(0.35, 0.35).setOrigin(0, 0);
 		bg.depth = -2;
 
-		
-		// ** CREACION DE INTERFAZ ** //
 		// crea el botón del movil
 		let movil = new Button(this, 850, 700, ' ', 2, 'movil', { "ClickCallback": () => this.ChangeScene("movil", scene), 
 																  "EnterCallback": () => this.OverMovile(),
